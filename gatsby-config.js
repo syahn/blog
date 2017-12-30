5042405963e59095d932fd9585a8f6b99e4fe44d
sangyoung.an <sangyoung.an@worksmobile.com>
2017-12-31 08:53:12 +0900

    Revert "Remove typographyJS"
    This reverts commit 1f32c7f65f82bc7c70f1b711510718f851258c38.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,12 @@ module.exports = {
     }
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,16 +1,16 @@
-// import Typography from "typography";
-// import "../assets/main.css";
+import Typography from "typography";
+import "../assets/main.css";
 
-// import usWebDesignStandardsTheme from "typography-theme-us-web-design-standards";
+import usWebDesignStandardsTheme from "typography-theme-us-web-design-standards";
 
-// usWebDesignStandardsTheme.baseColor = "rgba(0,0,0,.84)";
-// usWebDesignStandardsTheme.headerFontFamily = ["Source Sans Pro", "sans-serif"];
-// const typography = new Typography(usWebDesignStandardsTheme);
+usWebDesignStandardsTheme.baseColor = "rgba(0,0,0,.84)";
+usWebDesignStandardsTheme.headerFontFamily = ["Source Sans Pro", "sans-serif"];
+const typography = new Typography(usWebDesignStandardsTheme);
 
-// typography.overrideThemeStyles = ({ rhythm }, options) => ({
-//   p: {
-//     fontFamiliy: "KoPub Batang !important"
-//   }
-// });
+typography.overrideThemeStyles = ({ rhythm }, options) => ({
+  p: {
+    fontFamiliy: "KoPub Batang !important"
+  }
+});
 
-// export default typography;
+export default typography;
