4ec8f4134c63286abbb2f39f7f51df114fff75d6
sangyoung.an <sangyoung.an@worksmobile.com>
2017-12-31 10:20:19 +0900

    Add kopub font file

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,16 +1,10 @@
 import Typography from "typography";
+import 'font-kopub/css/kopubbatang.css';
 import "../assets/main.css";
-
 import usWebDesignStandardsTheme from "typography-theme-us-web-design-standards";
 
 usWebDesignStandardsTheme.baseColor = "rgba(0,0,0,.84)";
 usWebDesignStandardsTheme.headerFontFamily = ["Source Sans Pro", "sans-serif"];
+
 const typography = new Typography(usWebDesignStandardsTheme);
-
-typography.overrideThemeStyles = ({ rhythm }, options) => ({
-  p: {
-    fontFamiliy: "KoPub Batang !important"
-  }
-});
-
 export default typography;
