067472c3ba0441555ab474e77e0e7d26332f1025
sangyoung.an <sangyoung.an@worksmobile.com>
2017-12-31 19:30:27 +0900

    Change method of importing font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,6 @@
 import Typography from "typography";
-import 'font-kopub/css/kopubbatang.css';
-import "../assets/main.css";
+import "../assets/main.css"
+
 import usWebDesignStandardsTheme from "typography-theme-us-web-design-standards";
 
 usWebDesignStandardsTheme.baseColor = "rgba(0,0,0,.84)";
